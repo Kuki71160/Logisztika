4de77bb01ca02c5c52cfa4aff5e859f3b30350d8
Kuki71160 <szekeresbence06@gmail.com>
2025-02-18 00:38:54 +0100

    migrations_and_models_created

---
+++ app/Models/StockAdjustment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StockAdjustment extends Model
+{
+    use HasFactory;
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    protected $fillable = ['product_id', 'quantity', 'reason'];
+
+}
---
+++ database/seeders/Product_seeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Supplier;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class Product_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Supplier::insert([
+            [
+                'name' => 'Peter',
+                'contact' => 'Anyad',
+                'email' =>  'asd@asd.com'
+            ]
+        ]);
+
+        Category::insert([
+            [
+                'name' => 'Electronics',
+            ]
+        ]);
+
+        Product::insert([
+            [
+                'name' => 'Laptop',
+                'category_id' => 1,
+                'supplier_id' => 1,
+                'price' => 1200.00,
+                'stock' => 50,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
